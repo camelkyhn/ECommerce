8247ce901f7d977db05ab2f74349de67d134cfa2
Kemal Kayahan <kemal@mynextmatch.com>
2020-11-15 00:17:24 +0300

    E-1 Configured database context with entity relations.

--- ECommerce.Storage/DatabaseContext.cs
+++ ECommerce.Storage/DatabaseContext.cs
@@ -1,9 +1,48 @@
+using ECommerce.Middleware.Extensions;
+using ECommerce.Middleware.System;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Storage
 {
     public class DatabaseContext : DbContext
     {
-        
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region To Table
+
+            builder.ToCoreTables();
+
+            builder.ToIdentityTables();
+
+            #endregion
+
+            #region Relations
+
+            builder.AddCoreRelations();
+
+            builder.AddIdentityRelations();
+
+            builder.AddUpdatedUserRelations();
+
+            #endregion
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(EnvironmentVariable.DbConnectionString);
+            }
+        }
     }
 }
